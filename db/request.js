9f596605c3525050e8f93aaf6923d5628f504a84
Juanfunes19 <juan.funes1996@gmail.com>
2022-05-30 17:28:12 -0300

    Prueba de Heroku 1

--- db/request.js
+++ db/request.js
@@ -1,14 +1,26 @@
 const mysql = require(`mysql`)
+// const config = require(`./de.config`)
 
 
 module.exports.request = (query) => new Promise((res, rej) => {
     const connection = mysql.createConnection({
-        host:`localhost`,
+        host:  `localhost`,
         port: 3306,
         user: `root`,
         password: ``,
         database: `proyectofinal`
     })
+
+// module.exports.request = (query) => new Promise((res, rej) => {
+//     const connection = mysql.createPool({
+//         host: process.env.LOCAL ? `localhost` : config.HOST,
+//         port: 3306,
+//         user: process.env.LOCAL ? `root` : config.USER,
+//         password: process.env.LOCAL ? `` : config.PASSWORD,
+//         database: process.env.LOCAL ? `proyectofinal` : config.DB
+//     })
+
+
 
     connection.query(query,(error, data, fiels) =>{
         if(error) rej(error)
