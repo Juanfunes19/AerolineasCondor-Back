85e502c1b99fd81415248a374f37d885198eb27e
Juanfunes19 <juan.funes1996@gmail.com>
2022-05-30 12:01:36 -0300

    actualizacion base de datos

---
+++ db/de.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    USER: "b7f9e9f7850659",
+    PASSWORD: "dcbfc0a2",
+    HOST: "us-cdbr-east-05.cleardb.net",
+    DB: "heroku_c0ce37b37515e68"
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require(`express`)
 const app = express()
 const PORT = process.env.PORT || 8000
-const filmsRoutes = require(`./rutes/films`),
+const filmsRoutes = require(`./rutes/films`)
 
 app.use(express.json())
 
