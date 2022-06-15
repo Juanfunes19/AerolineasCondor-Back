4f12c18afa63549051e0847d169bf0b6c6c3bdfe
Juanfunes19 <juan.funes1996@gmail.com>
2022-06-15 17:55:04 -0300

    actualizacion 5.3

--- rutes/vuelos.js
+++ rutes/vuelos.js
@@ -1,7 +1,7 @@
 const express = require(`express`)
 const multer = require('multer')
 const router = express.Router()
-const molter = require('multer')
+const multer = require('multer')
 const upload = multer({dest: 'imagenes/'})
 const {allVuelosControllers, idVuelosControllers, deleteVuelosControllers, createVuelosControllers, updateVuelosControllers} = require(`../controllers/vuelosControllers`)
 
