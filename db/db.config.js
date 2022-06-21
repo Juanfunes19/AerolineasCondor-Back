e7906f91fe37f3ec0869fd2e86f4885dca6e7d3a
Juanfunes19 <juan.funes1996@gmail.com>
2022-06-21 19:17:37 -0300

    actualizacion6.1

--- db/db.config.js
+++ db/db.config.js
@@ -2,5 +2,9 @@ module.exports = {
     USER: "b7f9e9f7850659",
     PASSWORD: "dcbfc0a2",
     HOST: "us-cdbr-east-05.cleardb.net",
-    DB: "heroku_c0ce37b37515e68"
+    DB: "heroku_c0ce37b37515e68",
+
+    OAUTH_CLIENTID: "478266160588-vses66g4kp5v8k5ds2irvn4j0e24khbu.apps.googleusercontent.com",
+    OAUTH_CLIENTSECRET: "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",
+    REFRESH_TOKEN: "1//04LtXtP7cPipTCgYIARAAGAQSNwF-L9Ir-z45mQEh01Bfcka9ijLQlJbBDpVZWfgHKO54AmJME0pyJ8k9i5a-6vY1ASzVyHbu5Uk"
 }
--- utils.js
+++ utils.js
@@ -5,14 +5,31 @@ const nodemailer = require (`nodemailer`)
 module.exports.enviarEmail = (nombre, mail, mensaje) => new Promise((res, rej) => {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
-        host: "smtp.gmail.com", 
-        port: 587,
-        secure: false,
+        // host: "smtp.gmail.com", 
+        // port: 587,
+        // secure: false,
         auth:{
+            type: 'OAuth2',
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            pass: process.env.EMAIL_PASS,
+            clientId: process.env.ID_CLIENT,
+            clientSecret: process.env.ID_SEC,
+            refreshToken: process.env.REFRESH_TOKEN
         }
     })
+
+    // let transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         type: 'OAuth2',
+    //         user: credentials.MAIL_USERNAME,
+    //         pass: credentials.MAIL_PASSWORD,
+    //         clientId: credentials.OAUTH2_CLIENTID,
+    //         clientSecret: credentials.OAUTH2_CLIENT_SECRET,
+    //         refreshToken: credentials.OAUTH2_REFRESH_TOKEN
+    //     }
+    // });
+
 
     let mailOptions = {
         from: "aerolineascondor@gmail.com",
