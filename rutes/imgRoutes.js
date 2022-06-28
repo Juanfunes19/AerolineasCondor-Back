0aef67c69e736dcc3504a551f79453a130a61e67
Juanfunes19 <juan.funes1996@gmail.com>
2022-06-27 22:19:07 -0300

    actualizacion 10.4

--- rutes/imgRoutes.js
+++ rutes/imgRoutes.js
@@ -1,13 +1,13 @@
-const express = require(`express`)
-const router = express.Router()
-const { imgsController, imgController, createImgController, deleteImgController } = require('../controllers/imgControllers');
+// const express = require(`express`)
+// const router = express.Router()
+// const { imgsController, imgController, createImgController, deleteImgController } = require('../controllers/imgControllers');
 
-const multer = require('multer');
-const upload = multer({dest: 'skiImg/'});
+// const multer = require('multer');
+// const upload = multer({dest: 'skiImg/'});
 
-router.get('/', imgsController)
-router.get('/:key', imgController)
-router.post('/', upload.single('imagen'), createImgController)
-router.delete('/:path', deleteImgController)
+// router.get('/', imgsController)
+// router.get('/:key', imgController)
+// router.post('/', upload.single('imagen'), createImgController)
+// router.delete('/:path', deleteImgController)
 
-module.exports = router
+// module.exports = router
