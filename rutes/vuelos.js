90f6f4651d5b982229cf28899e9ecb10c2cf29c2
Juanfunes19 <juan.funes1996@gmail.com>
2022-06-30 18:57:20 -0300

    actualizacion 12.5

--- rutes/vuelos.js
+++ rutes/vuelos.js
@@ -2,11 +2,12 @@ const express = require(`express`)
 const router = express.Router()
 const {allVuelosControllers, idVuelosControllers, deleteVuelosControllers, createVuelosControllers, updateVuelosControllers} = require(`../controllers/vuelosControllers`)
 
-
+const multer = require('multer');
+const upload = multer({dest: 'skiImg/'});
 
 router.get(`/`, allVuelosControllers)
 router.get(`/:id`, idVuelosControllers)
-router.post(`/` , createVuelosControllers)
+router.post(`/`,upload.single('imagen'), createVuelosControllers)
 router.put(`/:id`, updateVuelosControllers)
 router.delete(`/:id`, deleteVuelosControllers)
 
