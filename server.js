9cbbc3b1fd2cf214c9dc6a94f9cadf9527bd5c15
Juanfunes19 <juan.funes1996@gmail.com>
2022-07-03 19:30:13 -0300

    actualizacion 16.0

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ const adminRoutes = require (`./rutes/adminRoutes`)
 // const imgRoutes = require (`./rutes/imgRoutes`)
 
 app.use(cors())
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+
 app.use(cookieParser())
 app.use(express.json())
 
